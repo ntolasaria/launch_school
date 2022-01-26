c1dd26983e5a8f26b59716d6e3e0b902788531c8
Nimish Tolasaria <ntolasaria@icloud.com>
2022-01-27 00:30:59 +0530

    rb_101 - random/word_count.rb edits

--- rb_101/rb_small_problems/random/word_count.rb
+++ rb_101/rb_small_problems/random/word_count.rb
@@ -3,10 +3,6 @@ text = File.read('example_problems.md')
 words = text.split(' ')
 puts "No. of word: #{words.size}"
 length = 0
-words.each do |word|
-  length += word.size
-end
-
+words.each { |word| length += word.size}
 average_length = length / words.size.to_f
-
 puts "Average word length is: #{average_length}"
