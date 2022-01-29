374f32ba74bb285aadca60d51173edeb9bfd2632
Nimish Tolasaria <ntolasaria@icloud.com>
2022-01-29 19:08:13 +0530

    rb_101 rb_small_problems - medium_1 - trial_3 part_5 to 10

---
+++ rb_101/rb_small_problems/medium_1/trial_3/part_10.rb
@@ -0,0 +1,14 @@
+def fibonacci_last(n)
+  num1, num2 = 1, 1
+  3.upto(n) do
+    num1, num2 = num2, (num1 + num2) % 10
+  end
+  num2
+end
+
+p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+p fibonacci_last(123456789) # -> 4
--- rb_101/rb_small_problems/medium_1/trial_3/part_5.rb
+++ rb_101/rb_small_problems/medium_1/trial_3/part_5.rb
@@ -25,4 +25,24 @@ def diamond(size)
   end
 end
 
+# Further Exploration :
+
+def print_hollow_star_line(size, offset)
+  spaces = size - 2 - (offset * 2)
+  if spaces < 0
+    puts '*'.center(size)
+  else
+    puts ('*' + ' '* spaces + '*').center(size)
+  end
+end
+
+def diamond_outline(size)
+  center_idx = size / 2
+  size.times do |idx|
+    offset = (center_idx - idx).abs
+    print_hollow_star_line(size, offset)
+  end
+end
+
 diamond(9)
+diamond_outline(9)
